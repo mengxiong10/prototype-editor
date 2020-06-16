d7fc485e9fce150d2716e37624a3ae3e0032d5be
mengxiong10 <15623530290@163.com>
2020-06-16 14:14:56 +0800

    refactor: 优化group代码

--- src/items/index.ts
+++ src/items/index.ts
@@ -7,11 +7,13 @@ import { textareaOptions } from './TextArea';
 import { checkboxOptions } from './Checkbox';
 import { radioOptions } from './Radio';
 
+const groupBase = '基础组件';
+
 registerComponent('rect', rectOptions);
 
-registerComponent('button', buttonOptions, { group: '基础组件', name: '按钮' });
-registerComponent('input', inputOptions, { group: '基础组件', name: '单行输入' });
-registerComponent('textarea', textareaOptions, { group: '基础组件', name: '多行输入' });
-registerComponent('rich', richEditorOptions, { group: '基础组件', name: '富文本' });
-registerComponent('checkbox', checkboxOptions, { group: '基础组件', name: '多选框' });
-registerComponent('radio', radioOptions, { group: '基础组件', name: '单选框' });
+registerComponent('button', buttonOptions, { group: groupBase, name: '按钮' });
+registerComponent('input', inputOptions, { group: groupBase, name: '单行输入' });
+registerComponent('textarea', textareaOptions, { group: groupBase, name: '多行输入' });
+registerComponent('rich', richEditorOptions, { group: groupBase, name: '富文本' });
+registerComponent('checkbox', checkboxOptions, { group: groupBase, name: '多选框' });
+registerComponent('radio', radioOptions, { group: groupBase, name: '单选框' });
